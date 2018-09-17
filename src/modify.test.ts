41664fccae250e4a0ea2e42ce518b6178ebe281c
Gordon <gburgett@watermark.org>
2018-09-17 16:28:10 -0500

    Create test-support for faking test data

--- src/modify.test.ts
+++ src/modify.test.ts
@@ -3,132 +3,54 @@ import {expect} from 'chai'
 import {IContentType} from './model'
 import {writeModify} from './modify'
 import { IContext } from './runners'
+import {fakeContentType, fakeField} from './test-support'
 import * as utils from './utils'
 
 describe('writeModify', () => {
-  const fromType: IContentType = {
-    sys: {
-      space: {
-        sys: {
-          type: 'Link',
-          linkType: 'Space',
-          id: '7yx6ovlj39n5',
-        },
-      },
-      id: 'menu',
-      type: 'ContentType',
-      createdAt: '2018-03-27T18:04:12.590Z',
-      updatedAt: '2018-04-06T20:02:28.519Z',
-      environment: {
-        sys: {
-          id: 'gburgett',
-          type: 'Link',
-          linkType: 'Environment',
-        },
-      },
-      createdBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      updatedBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      publishedCounter: 8,
-      version: 16,
-      publishedBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      publishedVersion: 15,
-      firstPublishedAt: '2018-03-27T18:04:12.855Z',
-      publishedAt: '2018-04-06T20:02:28.519Z',
-    },
-    displayField: 'name',
-    name: 'Menu',
-    description: 'A Menu contains a number of Menu Buttons or other Menus, which will be rendered as drop-downs.',
-    fields: [
-      {
-        id: 'name',
-        name: 'Menu Name',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [
-        ],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'topButton',
-        name: 'Top Button',
+  const fromType = fakeContentType('menu',
+    fakeField({
+      id: 'name',
+      name: 'Menu Name',
+      required: true,
+    }),
+    fakeField({
+      id: 'topButton',
+      name: 'Top Button',
+      type: 'Link',
+      validations: [ { linkContentType: ['menuButton'], message: 'The Top Button must be a...' }],
+      linkType: 'Entry',
+    }),
+    fakeField({
+      id: 'movedField',
+      name: 'Moved Field',
+      type: 'Number',
+      required: true,
+    }),
+    fakeField({
+      id: 'items',
+      name: 'Items',
+      type: 'Array',
+      items: {
         type: 'Link',
-        localized: false,
-        required: false,
         validations: [
           {
+            range: { min: 1, max: 4 },
+          },
+          {
             linkContentType: [
+              'menu',
               'menuButton',
             ],
-            message: 'The Top Button must be a...',
+            message: 'The items must be either buttons or drop-down menus',
           },
         ],
-        disabled: false,
-        omitted: false,
         linkType: 'Entry',
       },
-      {
-        id: 'movedField',
-        name: 'Moved Field',
-        type: 'Number',
-        localized: false,
-        required: true,
-        validations: [
-        ],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'items',
-        name: 'Items',
-        type: 'Array',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-        items: {
-          type: 'Link',
-          validations: [
-            {
-              range: { min: 1, max: 4 },
-            },
-            {
-              linkContentType: [
-                'menu',
-                'menuButton',
-              ],
-              message: 'The items must be either buttons or drop-down menus',
-            },
-          ],
-          linkType: 'Entry',
-        },
-      },
-      {
-        id: 'sideMenu',
-        name: 'Side Menu',
+    }),
+    fakeField({
+      id: 'sideMenu',
+      name: 'Side Menu',
         type: 'Link',
-        localized: false,
-        required: false,
         validations: [
           {
             linkContentType: [
@@ -137,145 +59,68 @@ describe('writeModify', () => {
             message: 'The Side Menu must be a Menu',
           },
         ],
-        disabled: false,
-        omitted: false,
         linkType: 'Entry',
-      },
-    ],
-  }
+    }),
+  )
 
-  const toType: IContentType = {
-    sys: {
-      space: {
-        sys: {
-          type: 'Link',
-          linkType: 'Space',
-          id: '7yx6ovlj39n5',
+  const toType = fakeContentType('menu',
+    fakeField({
+      id: 'name',
+      name: 'Menu Name',
+      type: 'Text',
+      required: true,
+    }),
+    fakeField({
+      id: 'movedField',
+      name: 'Moved Field',
+      type: 'Number',
+      validations: [
+        { in: [0, 1, 2] },
+      ],
+    }),
+    fakeField({
+      id: 'newField',
+      name: 'New Field',
+      type: 'Symbol',
+      required: true,
+    }),
+    fakeField({
+      id: 'topButton',
+      name: 'Top Button',
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: [
+            'menuButton',
+          ],
+          message: 'A new message',
         },
-      },
-      id: 'menu',
-      type: 'ContentType',
-      createdAt: '2018-03-27T18:04:12.590Z',
-      updatedAt: '2018-04-06T20:02:28.519Z',
-      environment: {
-        sys: {
-          id: 'gburgett',
-          type: 'Link',
-          linkType: 'Environment',
-        },
-      },
-      createdBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      updatedBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      publishedCounter: 8,
-      version: 16,
-      publishedBy: {
-        sys: {
-          type: 'Link',
-          linkType: 'User',
-          id: '0SUbYs2vZlXjVR6bH6o83O',
-        },
-      },
-      publishedVersion: 15,
-      firstPublishedAt: '2018-03-27T18:04:12.855Z',
-      publishedAt: '2018-04-06T20:02:28.519Z',
-    },
-    displayField: 'name',
-    name: 'Menu',
-    description: 'A Menu contains a number of Menu Buttons or other Menus, which will be rendered as drop-downs.',
-    fields: [
-      {
-        id: 'name',
-        name: 'Menu Name',
-        type: 'Text',
-        localized: false,
-        required: true,
-        validations: [
-        ],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'movedField',
-        name: 'Moved Field',
-        type: 'Number',
-        localized: false,
-        required: false,
-        validations: [
-          { in: [0, 1, 2] },
-        ],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'newField',
-        name: 'New Field',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [
-        ],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'topButton',
-        name: 'Top Button',
+      ],
+      linkType: 'Entry',
+    }),
+    fakeField({
+      id: 'items',
+      name: 'Items',
+      type: 'Array',
+      disabled: true,
+      items: {
         type: 'Link',
-        localized: false,
-        required: false,
         validations: [
           {
+            range: { min: 1, max: 5 },
+          },
+          {
             linkContentType: [
+              'menu',
               'menuButton',
             ],
-            message: 'A new message',
+            message: 'The items must be either buttons or drop-down menus',
           },
         ],
-        disabled: false,
-        omitted: false,
         linkType: 'Entry',
       },
-      {
-        id: 'items',
-        name: 'Items',
-        type: 'Array',
-        localized: false,
-        required: false,
-        validations: [
-        ],
-        disabled: true,
-        omitted: false,
-        items: {
-          type: 'Link',
-          validations: [
-            {
-              range: { min: 1, max: 5 },
-            },
-            {
-              linkContentType: [
-                'menu',
-                'menuButton',
-              ],
-              message: 'The items must be either buttons or drop-down menus',
-            },
-          ],
-          linkType: 'Entry',
-        },
-      },
-    ],
-  }
+    }),
+  )
 
   it('writes content type without def', async () => {
 
@@ -390,4 +235,5 @@ describe('writeModify', () => {
     expect(written.replace(/\s+/g, '')).to.include(`
   .validations([ { in: [0, 1, 2] } ])`.replace(/\s+/g, ''))
   })
+
 })
---
+++ src/test-support.ts
@@ -0,0 +1,70 @@
+import { IContentType, IField } from './model'
+
+export function fakeContentType(id: string = 'test', ...fields: IContentType['fields']): IContentType {
+ return {
+    sys: {
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: '7yx6ovlj39n5',
+        },
+      },
+      id,
+      type: 'ContentType',
+      createdAt: '2018-03-27T18:04:12.590Z',
+      updatedAt: '2018-04-06T20:02:28.519Z',
+      environment: {
+        sys: {
+          id: 'gburgett',
+          type: 'Link',
+          linkType: 'Environment',
+        },
+      },
+      createdBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: '0SUbYs2vZlXjVR6bH6o83O',
+        },
+      },
+      updatedBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: '0SUbYs2vZlXjVR6bH6o83O',
+        },
+      },
+      publishedCounter: 8,
+      version: 16,
+      publishedBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: '0SUbYs2vZlXjVR6bH6o83O',
+        },
+      },
+      publishedVersion: 15,
+      firstPublishedAt: '2018-03-27T18:04:12.855Z',
+      publishedAt: '2018-04-06T20:02:28.519Z',
+    },
+    displayField: 'name',
+    name: 'Menu',
+    description: 'A Menu contains a number of Menu Buttons or other Menus, which will be rendered as drop-downs.',
+    fields,
+  }
+}
+
+export function fakeField(field: Partial<IField>): IField {
+  return Object.assign({
+    id: 'test',
+    name: 'Test',
+    type: 'Symbol',
+    localized: false,
+    required: false,
+    validations: [
+    ],
+    disabled: false,
+    omitted: false,
+  }, field)
+}
