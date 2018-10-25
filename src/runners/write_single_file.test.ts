34835c5bf836a80a25aa69358ee2316c6fba2a17
Gordon <gburgett@watermark.org>
2018-10-25 10:18:57 -0500

    --one-file should write to specified filename
      If --out is a directory, behavior does not change.  If --out is a file
      or does not exist, --one-file causes the diff to be written to that
      file name.

---
+++ src/runners/write_single_file.test.ts
@@ -0,0 +1,64 @@
+import { expect } from 'chai'
+import * as fs from 'fs-extra'
+
+import { WriteSingleFileRunner } from './write_single_file'
+
+describe('WriteSingleFileRunner', () => {
+  describe('run', () => {
+
+    afterEach(async () => {
+      await fs.remove('temp.ts')
+      if (await fs.pathExists('db/migrate')) {
+        for (const f of await fs.readdir('db/migrate')) {
+          await fs.remove(`db/migrate/${f}`)
+        }
+        await fs.rmdir('db/migrate')
+      }
+    })
+
+    it('writes a chunk to the specified file', async () => {
+      const instance = new WriteSingleFileRunner('temp.ts', '', '')
+      await instance.init()
+      await instance.run(['k1', 'k2'],
+        async (id, write, ctx) => {
+          write(`${id}: test`)
+        },
+      )
+
+      await instance.close()
+
+      const contents = (await fs.readFile('temp.ts')).toString()
+
+      expect(contents).to.include('k1: test')
+      expect(contents).to.include('k2: test')
+    })
+
+    it('writes header and footer', async () => {
+      const instance = new WriteSingleFileRunner('temp.ts',
+        'HEADER!!!\n', 'FOOTER!!!\n')
+
+      await instance.init()
+
+      await instance.close()
+
+      const contents = (await fs.readFile('temp.ts')).toString()
+
+      expect(contents).to.include('HEADER!!!')
+      expect(contents).to.include('FOOTER!!!')
+    })
+
+    it('writes timestamped file if directory specified', async () => {
+      await fs.mkdirp('db/migrate')
+      const instance = new WriteSingleFileRunner('db/migrate',
+        'HEADER!!!\n', 'FOOTER!!!\n')
+
+      await instance.init()
+
+      await instance.close()
+
+      const files = await fs.readdir('db/migrate')
+      expect(files.length).to.eq(1)
+      expect(files[0]).to.match(/[0-9]+_generated_from_diff\.ts/)
+    })
+  })
+})
